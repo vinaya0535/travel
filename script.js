138350a81dba0884e56523e8b332f4b6f63cfc92
vinaya0535 <vaaher33@gmail.com>
2022-11-28 16:22:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".home-slider", {
+ 
+    loop:true,
+
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".pro-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".blogs", {
+    slidesPerView: 3,
+    loop:true,
+
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  
